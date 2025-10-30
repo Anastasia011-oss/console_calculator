30d1ba3a80c52b512865b4a362b62b38e9f9a054
Anastasia011-oss <kononenkosasa19@gmail.com>
2025-10-30 14:21:06 +0200

    Add base menu(Fix)

---
+++ gui.py
@@ -0,0 +1,18 @@
+def main():
+    print("=== Console Calculator ===")
+
+    while True:
+        try:
+            print("\n--- MENU ---")
+            print("1 - Addition (+)")
+            print("2 - Subtraction (-)")
+            print("3 - Multiplication (*)")
+            print("4 - Division (/)")
+            print("0 - Exit")
+
+            choice = input("Choose an option: ").strip()
+
+
+
+if __name__ == "__main__":
+    main()
