552d237ecb087e031c65484ab8075b1c2ba9df61
Anastasia011-oss <kononenkosasa19@gmail.com>
2025-10-30 13:42:52 +0200

    Add subtraction operation

--- calcul_2.py
+++ calcul_2.py
@@ -16,7 +16,7 @@ def main():
                 print("Exiting the calculator.")
                 break
 
-            if choice == '1':  # Addition
+            if choice == '1':
                 num1_input = input("Enter the first number: ").strip()
                 num1 = float(num1_input)
 
@@ -26,8 +26,20 @@ def main():
                 result = num1 + num2
                 print(f"Result: {num1} + {num2} = {result}")
 
+            elif choice == '2':
+                num1_input = input("Enter the first number: ").strip()
+                num1 = float(num1_input)
+
+                num2_input = input("Enter the second number: ").strip()
+                num2 = float(num2_input)
+
+                result = num1 - num2
+                print(f"Result: {num1} - {num2} = {result}")
+
         except ValueError:
             print("Error: please enter valid numbers!")
+
+
 
 
 if __name__ == "__main__":
